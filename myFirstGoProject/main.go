5012b3cd00ae9fd216f491c23631fe01dea9afbf
Bruno Tatsch <brunotatsch@gmail.com>
2025-06-12 17:15:49 -0300

    Conteúdo 2 - Ambiente

---
+++ myFirstGoProject/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!!!")
+}
+
+// Execute ontime
+// # go run main.go
+
+// Create executable
+// # go build main.go
+
+// Create executable with name
+// # go build -o "hello" main.go
+
+// Create executable with crosscompilation (to another s.o)
+// # go build -o "hello" main.go
